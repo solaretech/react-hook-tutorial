61ef31393b2ec407fea6a8ee4d707419a4f1334b
solaretech <solaretech.kk@gmail.com>
2022-06-19 17:56:41 +0900

    Add Editing BookRow

--- src/App.tsx
+++ src/App.tsx
@@ -32,6 +32,15 @@ const dummyBooks: BookToRead[] = [
 const App = () => {
   const [books, setBooks] = useState(dummyBooks)
 
+  const handleBookMemoChange = (id: number, memo: string) => {
+    const newBooks = books.map((b) => {
+      return b.id === id
+        ? {...b, memo: memo}
+        : b;
+    })
+    setBooks(newBooks)
+  }
+
   const handleBookDelete = (id: number) => {
     const newBooks = books.filter((b) => b.id != id)
     setBooks(newBooks)
@@ -42,7 +51,7 @@ const App = () => {
       <BookRow 
         book={b}
         key={b.id}
-        onMemoChange={(id, memo) => {}}
+        onMemoChange={(id, memo) => handleBookMemoChange(id, memo)}
         onDelete={(id) => handleBookDelete(id)}
       />
     )
